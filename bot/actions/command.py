914630a856dc2a83d792f3f0109c9466c60f1d71
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2016-06-30 19:51:49 +0200

    Improve Command support
     Saving command_args in event

--- bot/actions/command.py
+++ bot/actions/command.py
@@ -9,9 +9,12 @@ class CommandAction(IntermediateAction):
         self.command = command
 
     def post_setup(self):
-        self.command_pattern = re.compile("^/" + self.command + "(@" + self.cache.bot_info.username + "| |$)", re.IGNORECASE)
+        self.command_pattern = re.compile("(^/" + self.command + "(@" + self.cache.bot_info.username + "| |$)) *(.*)",
+                                          re.IGNORECASE)
 
     def process(self, event):
         match = self.command_pattern.match(event.text)
         if match is not None:
+            event.command = match.group(1).rstrip()
+            event.command_args = match.group(3)
             self._continue(event)
