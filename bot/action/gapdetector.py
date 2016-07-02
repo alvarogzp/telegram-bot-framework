afcd3e89722ab9558b357b1337d58d6e53437734
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2016-07-02 12:53:37 +0200

    Add GapDetectorAction

---
+++ bot/action/gapdetector.py
@@ -0,0 +1,20 @@
+from bot.action.core.action import IntermediateAction
+
+
+class GapDetectorAction(IntermediateAction):
+    def post_setup(self):
+        self.gap_state = self.state.get_for("gap")
+
+    def process(self, event):
+        expected_update_id = self.get_expected_update_id()
+        current_update_id = event.update.update_id
+        self.gap_state.last_update_id = str(current_update_id)
+        if expected_update_id is None or current_update_id != expected_update_id:
+            print("Gap detected!")
+            event.gap_detected = True
+        self._continue(event)
+
+    def get_expected_update_id(self):
+        last_update_id = self.gap_state.last_update_id
+        if last_update_id is not None:
+            return int(last_update_id) + 1
