ec8191ce4f2af310b22e1774f77ec875a59c9b4e
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2016-07-08 17:19:11 +0200

    Refactor HashtagListAction
    Now it display recent or popular hashtags with a limit on the number of hashtags shown.

--- bot/action/extra/hashtags.py
+++ bot/action/extra/hashtags.py
@@ -1,6 +1,9 @@
 import collections
 
 from bot.action.core.action import Action
+from bot.action.core.command import CommandUsageMessage
+from bot.action.userinfo import UserStorageHandler
+from bot.action.util.format import DateFormatter, UserFormatter
 from bot.api.domain import Message, MessageEntityParser
 
 
@@ -33,16 +36,66 @@ class HashtagRecolectorAction(Action):
 
 class HashtagListAction(Action):
     def process(self, event):
-        hashtags = HashtagStorageHandler(event).get_stored_hashtags()
-        response = self.get_response_message(hashtags)
-        self.api.send_message(Message.create_reply(event.message, response))
+        action, number_of_hashtags_to_display, help_args = self.parse_args(event.command_args.split())
+        if action in ("recent", "popular"):
+            hashtags = HashtagStorageHandler(event).get_stored_hashtags()
+            if hashtags.is_empty():
+                response = self.get_response_empty()
+            elif action == "recent":
+                response = self.get_response_recent(event, hashtags, number_of_hashtags_to_display)
+            else:
+                response = self.get_response_popular(event, hashtags, number_of_hashtags_to_display)
+        else:
+            response = self.get_response_help(event, help_args)
+        self.api.send_message(response.replying_to(event.message))
 
     @staticmethod
-    def get_response_message(hashtags):
-        if hashtags.is_empty():
-            return "I have not seen any hashtag in this chat.\nWrite some and try again (hint: #ThisIsAHashTag)."
-        formatted_hashtags = ("%s -> %s" % (count, hashtag) for hashtag, count in hashtags.counted_by_popularity())
-        return "\n".join(formatted_hashtags)
+    def parse_args(args):
+        action = "help"
+        number_of_hashtags_to_display = 10
+        help_args = args[1:]
+        if len(args) == 0:
+            action = "recent"
+        elif len(args) == 1:
+            if args[0].isnumeric():
+                action = "recent"
+                number_of_hashtags_to_display = int(args[0])
+            else:
+                action = args[0]
+        elif len(args) == 2:
+            if args[1].isnumeric():
+                number_of_hashtags_to_display = int(args[1])
+                action = args[0]
+        return action, number_of_hashtags_to_display, help_args
+
+    @staticmethod
+    def get_response_help(event, help_args):
+        args = "[number_of_hashtags]|[popular [number_of_hashtags]]"
+        description = "By default, display recent hashtags.\n" \
+                      "Use *popular* to show most popular ones.\n" \
+                      "You can also add a number to the end in both modes to limit the hashtags to display" \
+                      " (default is 10)."
+        return CommandUsageMessage.get_usage_message(event.command, args, description)
+
+    @staticmethod
+    def get_response_empty():
+        return Message.create("I have not seen any hashtag in this chat.\n"
+                              "Write some and try again (hint: #ThisIsAHashTag).")
+
+    def get_response_recent(self, event, hashtags, number_of_hashtags_to_display):
+        user_storage_handler = UserStorageHandler.get_instance(self.state)
+        sorted_hashtags = hashtags.sorted_by_recent_use(number_of_hashtags_to_display)
+        printable_hashtags = sorted_hashtags.printable_version(user_storage_handler)
+        return self.__build_success_response_message(event, "Most recent hashtags:", printable_hashtags)
+
+    def get_response_popular(self, event, hashtags, number_of_hashtags_to_display):
+        printable_hashtags = hashtags.grouped_by_popularity(number_of_hashtags_to_display).printable_version()
+        return self.__build_success_response_message(event, "Most popular hashtags:", printable_hashtags)
+
+    @staticmethod
+    def __build_success_response_message(event, title, printable_hashtags):
+        footer = "\n\nUse *" + event.command + " help* to see more options."
+        return Message.create(title + "\n" + printable_hashtags + footer, parse_mode="Markdown")
 
 
 class Hashtag:
@@ -50,6 +103,11 @@ class Hashtag:
         self.hashtag = hashtag
         self.date = date
         self.user_id = user_id
+
+    def printable_version(self, user_storage_handler):
+        formatted_date = DateFormatter.format(self.date) if self.date is not None else "???"
+        formatted_user = UserFormatter.format(user_storage_handler.get(self.user_id)) if self.user_id is not None else "???"
+        return "%s  (%s by %s)" % (self.hashtag, formatted_date, formatted_user)
 
     def serialize(self):
         return "%s %s %s\n" % (self.hashtag, self.date, self.user_id)
@@ -69,9 +127,18 @@ class HashtagList:
     def is_empty(self):
         return len(self.hashtags) == 0
 
-    def counted_by_popularity(self):
+    def grouped_by_popularity(self, max_to_return):
         hashtags_names = (hashtag.hashtag for hashtag in self.hashtags)
-        return collections.Counter(hashtags_names).most_common()
+        return HashtagGroup(collections.Counter(hashtags_names).most_common(max_to_return))
+
+    def sorted_by_recent_use(self, limit):
+        if limit <= 0:
+            return HashtagList([])
+        # for now, assume they are already sorted by date
+        return HashtagList(reversed(self.hashtags[-limit:]))
+
+    def printable_version(self, user_storage_handler):
+        return "\n".join((hashtag.printable_version(user_storage_handler) for hashtag in self.hashtags))
 
     def serialize(self):
         return "".join((hashtag.serialize() for hashtag in self.hashtags))
@@ -79,6 +146,14 @@ class HashtagList:
     @staticmethod
     def deserialize(hashtags_data):
         return HashtagList([Hashtag.deserialize(hashtag) for hashtag in hashtags_data.splitlines()])
+
+
+class HashtagGroup:
+    def __init__(self, grouped_hashtags):
+        self.grouped_hashtags = grouped_hashtags
+
+    def printable_version(self):
+        return "\n".join(("%s -> %s" % (count, hashtag) for hashtag, count in self.grouped_hashtags))
 
 
 class HashtagStorageHandler:
