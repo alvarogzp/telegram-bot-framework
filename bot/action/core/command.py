30d0e07da3fd8a34225b8e3ed98474418108ba4a
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2016-07-20 06:06:59 +0200

    Update CommandAction matcher
    To use message entities instead of trying to match every message with the command regex

--- bot/action/core/command.py
+++ bot/action/core/command.py
@@ -1,7 +1,7 @@
 import re
 
 from bot.action.core.action import IntermediateAction
-from bot.api.domain import Message
+from bot.api.domain import Message, MessageEntityParser
 
 
 class CommandAction(IntermediateAction):
@@ -10,15 +10,34 @@ class CommandAction(IntermediateAction):
         self.command = command
 
     def post_setup(self):
-        self.command_pattern = re.compile("(^/" + self.command + "(@" + self.cache.bot_info.username + "| |$)) *(.*)",
+        self.command_pattern = re.compile("^/" + self.command + "(@" + self.cache.bot_info.username + ")?$",
                                           re.IGNORECASE)
 
     def process(self, event):
-        match = self.command_pattern.match(event.text)
-        if match is not None:
-            event.command = match.group(1).rstrip()
-            event.command_args = match.group(3)
-            self._continue(event)
+        for entity in self.get_entities(event):
+            if self.is_valid_command(entity):
+                parser = MessageEntityParser(event.message)
+                command_text = parser.get_entity_text(entity)
+                if self.matches_expected_command(command_text):
+                    event.command = command_text
+                    event.command_args = self.get_command_args(parser, entity)
+                    self._continue(event)
+
+    @staticmethod
+    def get_entities(event):
+        entities = event.message.entities
+        return entities if entities is not None else []
+
+    @staticmethod
+    def is_valid_command(entity):
+        return entity.type == "bot_command" and entity.offset == 0
+
+    def matches_expected_command(self, command_text):
+        return self.command_pattern.match(command_text) is not None
+
+    @staticmethod
+    def get_command_args(parser, entity):
+        return parser.get_text_after_entity(entity)[1:]
 
 
 class CommandUsageMessage:
