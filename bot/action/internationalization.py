892b1f04ba6f6dde0953c061409fb4eb05935634
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2016-09-30 02:49:25 +0200

    Make CACHED_TRANSLATIONS global, fix get_value typo

--- bot/action/internationalization.py
+++ bot/action/internationalization.py
@@ -7,24 +7,27 @@ TRANSLATION_DOMAIN = "telegram-bot"
 
 DEFAULT_LANGUAGE = "en"
 
+CACHED_TRANSLATIONS = {}
+
 
 class InternationalizationAction(IntermediateAction):
     def __init__(self):
         super().__init__()
-        self.cached_translations = {}
         self.default_translation = self.__get_translation(DEFAULT_LANGUAGE)
 
     def process(self, event):
-        lang = event.state.get_for("settings").get("language", DEFAULT_LANGUAGE)
+        lang = event.state.get_for("settings").get_value("language", DEFAULT_LANGUAGE)
         translation = self.__get_translation(lang)
         translation.install()
         event._ = translation.gettext
         self._continue(event)
         self.default_translation.install()
 
-    def __get_translation(self, language):
-        if language in self.cached_translations:
-            return self.cached_translations[language]
-        translation = gettext.translation(TRANSLATION_DOMAIN, LOCALE_DIR, languages=[language], fallback=True)
-        self.cached_translations[language] = translation
+    @staticmethod
+    def __get_translation(language):
+        if language in CACHED_TRANSLATIONS:
+            translation = CACHED_TRANSLATIONS[language]
+        else:
+            translation = gettext.translation(TRANSLATION_DOMAIN, LOCALE_DIR, languages=[language], fallback=True)
+            CACHED_TRANSLATIONS[language] = translation
         return translation
