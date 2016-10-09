c6a02a1559b9def22ff9c838b609bbacd60a0507
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2016-10-09 07:24:15 +0200

    Make internationalized actions explicit enable it passing a domain, so that different actions can have different domains, decoupling internationalized actions from each other.

--- bot/action/internationalization.py
+++ bot/action/internationalization.py
@@ -3,31 +3,53 @@ import gettext
 from bot.action.core.action import IntermediateAction
 
 LOCALE_DIR = "locales"
-TRANSLATION_DOMAIN = "telegram-bot"
 
+DEFAULT_DOMAIN = "telegram-bot"
 DEFAULT_LANGUAGE = "en"
 
 CACHED_TRANSLATIONS = {}
 
 
+def get_translation(domain, language):
+    key = (domain, language)
+    translation = CACHED_TRANSLATIONS.get(key)
+    if translation is None:
+        translation = gettext.translation(domain, LOCALE_DIR, languages=[language], fallback=True)
+        CACHED_TRANSLATIONS[key] = translation
+    return translation
+
+
 class InternationalizationAction(IntermediateAction):
     def __init__(self):
         super().__init__()
-        self.default_translation = self.__get_translation(DEFAULT_LANGUAGE)
+        self.default_translation = get_translation(DEFAULT_DOMAIN, DEFAULT_LANGUAGE)
 
     def process(self, event):
-        lang = event.state.get_for("settings").get_value("language", DEFAULT_LANGUAGE)
-        translation = self.__get_translation(lang)
-        translation.install()
-        event._ = translation.gettext
+        event.i18n = InternationalizationHelper
         self._continue(event)
-        self.default_translation.install()
+        if InternationalizationHelper.is_enabled(event):
+            InternationalizationHelper.disable(event)
+            self.default_translation.install()
+        event.i18n = None
+
+
+class InternationalizationHelper:
+    @classmethod
+    def enable(cls, event, domain):
+        language = cls._get_language(event)
+        translation = get_translation(domain, language)
+        translation.install()
+        _ = event._ = translation.gettext
+        return _
 
     @staticmethod
-    def __get_translation(language):
-        if language in CACHED_TRANSLATIONS:
-            translation = CACHED_TRANSLATIONS[language]
-        else:
-            translation = gettext.translation(TRANSLATION_DOMAIN, LOCALE_DIR, languages=[language], fallback=True)
-            CACHED_TRANSLATIONS[language] = translation
-        return translation
+    def _get_language(event):
+        return event.state.get_for("settings").get_value("language", DEFAULT_LANGUAGE)
+
+    @staticmethod
+    def is_enabled(event):
+        return event._ is not None
+
+    @staticmethod
+    def disable(event):
+        event._ = None
