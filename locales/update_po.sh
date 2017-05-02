87552cf4ce358866e91fbd7c4885791965978b0d
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-05-02 04:31:51 +0200

    Merge existing po files in addition to creating a pot file

--- locales/update_po.sh
+++ locales/update_po.sh
@@ -25,12 +25,30 @@ generate_pot_file()
     xgettext "$file" --from-code=utf-8 --default-domain="$domain" --output="$domain.pot" --output-dir="$LOCALES_DIR"
 }
 
-generate_pot_files()
+update_po_files()
 {
-    generate_pot_file pole bot/action/extra/pole.py
+    local domain="$1"
+    local pot_path="$LOCALES_DIR/$domain.pot"
+    for file in ${LOCALES_DIR}/*/LC_MESSAGES/${domain}.po
+    do
+        msgmerge --update --no-fuzzy-matching --verbose "$file" "$pot_path"
+    done
+}
+
+generate_pot_file_and_update_po_files()
+{
+    local domain="$1"
+    local file="$2"
+    generate_pot_file "$domain" "$file"
+    update_po_files "$domain"
+}
+
+generate_pot_and_update_po_files()
+{
+    generate_pot_file_and_update_po_files pole bot/action/extra/pole.py
 }
 
 
 cd_to_project_root
 
-generate_pot_files
+generate_pot_and_update_po_files
