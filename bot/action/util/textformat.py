283eee75432080aee760d69cc11bd814b8f6b10f
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-05-02 22:53:26 +0200

    Fix bug in FormattedText.join

--- bot/action/util/textformat.py
+++ bot/action/util/textformat.py
@@ -43,6 +43,7 @@ class FormattedText:
 
     def join(self, formatted_texts):
         """:type formatted_texts: list[FormattedText]"""
+        formatted_texts = list(formatted_texts)  # so that after the first iteration elements are not lost if generator
         for formatted_text in formatted_texts:
             assert self.mode == formatted_text.mode, "Cannot join text with different modes"
         self.text = self.text.join((formatted_text.text for formatted_text in formatted_texts))
