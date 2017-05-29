5099288fc48211f490e90525f4c1beb8ce62109c
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-05-29 08:18:56 +0200

    Use mapper from stored_message.py in MessageStorageHandler

--- bot/action/extra/messages/__init__.py
+++ bot/action/extra/messages/__init__.py
@@ -6,6 +6,7 @@ from bot.action.core.action import Action
 from bot.action.core.command import UnderscoredCommandBuilder
 from bot.action.core.command.usagemessage import CommandUsageMessage
 from bot.action.extra.messages import analyzer
+from bot.action.extra.messages.stored_message import StoredMessageMapper
 from bot.action.userinfo import UserStorageHandler
 from bot.action.util.format import UserFormatter, DateFormatter
 from bot.action.util.textformat import FormattedText
@@ -376,42 +377,9 @@ class MessageStorageHandler:
         return MessageList(self.state.list_keys(), self.state)
 
     def save_message(self, message):
-        data = message.data.copy()
-        self.__replace_with_id_if_present(data, "from")
-        self.__replace_with_id_if_present(data, "forward_from")
-        self.__replace_with_id_if_present(data, "reply_to_message", "message_id")
-        self.__delete_if_present(data, "chat")
-        self.__delete_if_present(data, "message_id")
-        self.__delete_if_present(data, "entities")
-        self.__replace_list_with_item_with_biggest(data, "photo", "height")
-        self.__delete_if_present(data.get("sticker"), "thumb")
-        self.__delete_if_present(data.get("document"), "thumb")
-        self.__delete_if_present(data.get("video_note"), "thumb")
-        self.__delete_if_present(data.get("video"), "thumb")
+        data = StoredMessageMapper.from_api(message).map().to_data()
         dump = json.dumps(data)
         self.state.set_value(str(message.message_id), dump + "\n", append=True)
-
-    def __replace_with_id_if_present(self, dict, key, id_key="id"):
-        if self.__is_present(dict, key):
-            dict[key] = dict[key][id_key]
-
-    @staticmethod
-    def __delete_if_present(dict, key):
-        if dict is not None:
-            dict.pop(key, None)
-
-    def __replace_list_with_item_with_biggest(self, dict, key, attr):
-        if self.__is_present(dict, key):
-            biggest = None
-            for item in dict[key]:
-                if biggest is None or item[attr] >= biggest[attr]:
-                    biggest = item
-            if biggest is not None:
-                dict[key] = biggest
-
-    @staticmethod
-    def __is_present(dict, key):
-        return dict.get(key) is not None
 
     def delete_old_messages(self):
         stored_ids = self.state.list_keys()
