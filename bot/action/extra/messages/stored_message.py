8b70b6de5102f8df731121f6e8ebe71a42e9ce84
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-05-29 08:12:24 +0200

    Improve class StoredMessageMapper

--- bot/action/extra/messages/stored_message.py
+++ bot/action/extra/messages/stored_message.py
@@ -1,18 +1,28 @@
 class StoredMessageMapper:
-    def from_api(self, message):
+    def __init__(self, data):
+        self.data = data
+
+    @staticmethod
+    def from_api(message):
         data = message.data.copy()
-        self.__replace_with_id_if_present(data, "from")
-        self.__replace_with_id_if_present(data, "forward_from")
-        self.__replace_with_id_if_present(data, "reply_to_message", "message_id")
-        self.__delete_if_present(data, "chat")
-        self.__delete_if_present(data, "message_id")
-        self.__delete_if_present(data, "entities")
-        self.__replace_list_with_item_with_biggest(data, "photo", "height")
-        self.__delete_if_present(data.get("sticker"), "thumb")
-        self.__delete_if_present(data.get("document"), "thumb")
-        self.__delete_if_present(data.get("video_note"), "thumb")
-        self.__delete_if_present(data.get("video"), "thumb")
-        return data
+        return StoredMessageMapper(data)
+
+    def map(self):
+        self.__replace_with_id_if_present(self.data, "from")
+        self.__replace_with_id_if_present(self.data, "forward_from")
+        self.__replace_with_id_if_present(self.data, "reply_to_message", "message_id")
+        self.__delete_if_present(self.data, "chat")
+        self.__delete_if_present(self.data, "message_id")
+        self.__delete_if_present(self.data, "entities")
+        self.__replace_list_with_item_with_biggest(self.data, "photo", "height")
+        self.__delete_if_present(self.data.get("sticker"), "thumb")
+        self.__delete_if_present(self.data.get("document"), "thumb")
+        self.__delete_if_present(self.data.get("video_note"), "thumb")
+        self.__delete_if_present(self.data.get("video"), "thumb")
+        return self
+
+    def to_data(self):
+        return self.data
 
     def __replace_with_id_if_present(self, data, key, id_key="id"):
         if self.__is_present(data, key):
