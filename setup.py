55a640fef97b240e6246de674e117227012168c3
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-10-07 07:16:22 +0200

    Add setup.py to allow telegram-bot to be installed as a dependency

---
+++ setup.py
@@ -0,0 +1,39 @@
+import os
+
+from setuptools import setup
+
+
+def get_version_from_git_most_recent_tag():
+    return os.popen("git tag -l v* | tail --lines=1").read().strip().lstrip("v")
+
+
+def get_readme_content():
+    current_file_dir = os.path.dirname(__file__)
+    readme_file_path = os.path.join(current_file_dir, "README.md")
+    return open(readme_file_path).read()
+
+
+setup(
+    name='telegram-bot',
+
+    version=get_version_from_git_most_recent_tag(),
+
+    description='Python Telegram bot API framework',
+    long_description=get_readme_content(),
+
+    url='https://github.com/alvarogzp/telegram-bot',
+
+    author='Alvaro Gutierrez Perez',
+    author_email='alvarogzp@gmail.com',
+
+    license='GPL-3.0',
+
+    packages=['bot'],
+
+    install_requires=[
+        'requests',
+        'pytz'
+    ],
+
+    python_requires='>=3',
+)
