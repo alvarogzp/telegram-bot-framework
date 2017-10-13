d1605825259b3ebdb8426f44e80ebde91cddc957
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-10-13 19:34:04 +0200

    Make Logger tags dynamic

--- bot/logger/logger.py
+++ bot/logger/logger.py
@@ -4,13 +4,13 @@ from bot.logger.reusable_message_sender import MessageSender
 
 
 class Logger:
-    def __init__(self, sender: MessageSender, tag: str = ""):
+    def __init__(self, sender: MessageSender):
         self.sender = sender
-        self.tag = tag
 
-    def log(self, text):
-        text = self.__get_formatted_text(text)
+    def log(self, tag, text):
+        text = self.__get_formatted_text(tag, text)
         self.sender.send(text)
 
-    def __get_formatted_text(self, text):
-        return "{time} [{tag}] {text}".format(time=time.strftime("%X"), tag=self.tag, text=text)
+    @staticmethod
+    def __get_formatted_text(tag, text):
+        return "{time} [{tag}] {text}".format(time=time.strftime("%X"), tag=tag, text=text)
