22c1c2723c6371accad08540b6e6c11d13b90f9e
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-10-13 19:45:50 +0200

    Use new admin logger in bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -1,10 +1,7 @@
-import traceback
-
 from bot.action.core.action import Update, Action
 from bot.api.api import Api
-from bot.api.domain import Message
 from bot.api.telegram import TelegramBotApi
-from bot.logger.error_handler import ErrorHandler
+from bot.logger.admin_logger import AdminLogger
 from bot.multithreading.scheduler import SchedulerApi
 from bot.storage import Config, Cache
 from bot.storage import State
@@ -22,8 +19,8 @@ class Bot:
         debug = self.config.is_debug_enabled()
         telegram_api = TelegramBotApi(self.config.auth_token, debug)
         self.api = Api(telegram_api, self.state)
-        self.error_handler = ErrorHandler(self.api, self.config.admin_chat_id, debug)
-        self.scheduler = SchedulerApi(self.error_handler.handle_work_error)
+        self.logger = AdminLogger(self.api, self.config.admin_chat_id, debug)
+        self.scheduler = SchedulerApi(self.logger.work_error)
         self.scheduler.setup()
         self.cache.bot_info = self.api.getMe()
         self.action = Action()
@@ -33,38 +30,39 @@ class Bot:
         self.action = action
 
     def run(self):
-        self.send_to_admin("Started")
+        self.logger.info("Started")
         try:
             self.main_loop()
         except KeyboardInterrupt:
-            self.send_to_admin("KeyboardInterrupt")
+            self.logger.info("KeyboardInterrupt")
         except SystemExit as e:
-            self.send_to_admin("SystemExit: " + str(e))
+            self.logger.info("SystemExit: " + str(e))
             raise e
         except BaseException as e:
-            self.send_to_admin("Fatal error: " + str(e))
+            self.logger.error(e, "Fatal error")
             raise e
         finally:
             self.shutdown()
 
     def main_loop(self):
-        for update in self.api.get_pending_updates():
+        for update in self.get_updates(self.api.get_pending_updates):
             self.process_update(update, is_pending=True)
         while True:
-            for update in self.api.get_updates():
+            for update in self.get_updates(self.api.get_updates):
                 self.process_update(update)
+
+    def get_updates(self, get_updates_func):
+        try:
+            return get_updates_func()
+        except Exception as e:
+            self.logger.error(e, "get_updates")
 
     def process_update(self, update, is_pending=False):
         try:
             self.action.process(Update(update, is_pending))
         except Exception as e:
-            self.send_to_admin("Error while processing update. Action " + self.action.get_name() + " failed with error: " + str(e))
-            traceback.print_exc()
+            self.logger.error(e, "process_update")
 
     def shutdown(self):
         self.scheduler.shutdown()
-        self.send_to_admin("Finished")
-
-    def send_to_admin(self, message):
-        message_to_admin = "[admin] " + message
-        self.api.send_message(Message.create(text=message_to_admin, chat_id=self.config.admin_user_id))
+        self.logger.info("Finished")
