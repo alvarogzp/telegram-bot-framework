d9759e9baf86f1c5e374a6118f9efac2594d46bb
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-10-14 13:34:39 +0200

    Update Logger and AdminLogger to use FormattedText using the FormattedTextReusableMessageSender

--- bot/logger/admin_logger.py
+++ bot/logger/admin_logger.py
@@ -1,25 +1,28 @@
 import traceback
 
+from bot.action.util.textformat import FormattedText
 from bot.api.api import Api
 from bot.logger.logger import Logger
-from bot.logger.reusable_message_sender import TimedReusableMessageSender, ReusableMessageSender
-from bot.multithreading.worker import Worker
+from bot.logger.reusable_message_sender import TimedReusableMessageSender, FormattedTextReusableMessageSender
 from bot.multithreading.work import Work
+from bot.multithreading.worker import Worker
 
 
 class AdminLogger:
     def __init__(self, api: Api, admin_chat_id: str, debug: bool):
-        sender = TimedReusableMessageSender(ReusableMessageSender(api, admin_chat_id), reuse_message_for_seconds=5)
+        sender = TimedReusableMessageSender(FormattedTextReusableMessageSender(api, admin_chat_id),
+                                            reuse_message_for_seconds=1)
         self.logger = Logger(sender)
         self.debug = debug
 
     def work_error(self, error: BaseException, work: Work, worker: Worker):
-        text = "Worker: {worker} | Work: {work} | {error}"\
-            .format(worker=worker.name, work=work.name, error=str(error))
+        text = FormattedText().normal("Worker: {worker} | Work: {work} | {error}")\
+            .start_format().bold(worker=worker.name, work=work.name, error=str(error)).end_format()
         self.__error(text)
 
     def error(self, error: BaseException, action: str):
-        text = "Action: {action} | {error}".format(action=action, error=str(error))
+        text = FormattedText().normal("Action: {action} | {error}")\
+            .start_format().bold(action=action, error=str(error)).end_format()
         self.__error(text)
 
     def __error(self, text):
@@ -31,7 +34,7 @@ class AdminLogger:
             traceback.print_exc()
 
     def info(self, info_text: str):
-        self.__info(info_text)
+        self.__info(FormattedText().bold(info_text))
 
     def __info(self, text):
         self.logger.log("INFO", text)
--- bot/logger/logger.py
+++ bot/logger/logger.py
@@ -1,5 +1,6 @@
 import time
 
+from bot.action.util.textformat import FormattedText
 from bot.logger.reusable_message_sender import MessageSender
 
 
@@ -7,10 +8,11 @@ class Logger:
     def __init__(self, sender: MessageSender):
         self.sender = sender
 
-    def log(self, tag, text):
+    def log(self, tag: str, text: FormattedText):
         text = self.__get_formatted_text(tag, text)
         self.sender.send(text)
 
     @staticmethod
-    def __get_formatted_text(tag, text):
-        return "{time} [{tag}] {text}".format(time=time.strftime("%X"), tag=tag, text=text)
+    def __get_formatted_text(tag: str, text: FormattedText):
+        return FormattedText().normal("{time} [{tag}] {text}").start_format()\
+            .normal(time=time.strftime("%X")).bold(tag=tag).concat(text=text).end_format()
