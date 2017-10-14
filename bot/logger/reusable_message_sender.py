2332fa94c18a3e1781c9d93bfcd1c4839c0a5678
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-10-14 13:08:10 +0200

    Create a FormattedTextReusableMessageSender to allow formatted texts to be used as input for message sender
    The previous one is retained in PlainTextReusableMessageSender, which shares with the formatted text one a ReusableMessageSender abstract class

--- bot/logger/reusable_message_sender.py
+++ bot/logger/reusable_message_sender.py
@@ -1,5 +1,6 @@
 import time
 
+from bot.action.util.textformat import FormattedText
 from bot.api.api import Api
 from bot.api.domain import Message
 
@@ -10,31 +11,74 @@ class MessageSender:
 
 
 class ReusableMessageSender(MessageSender):
-    def __init__(self, api: Api, chat_id, separator: str = "\n\n"):
+    def __init__(self, api: Api, separator):
         self.api = api
-        self.message = Message.create("", chat_id)
         self.separator = separator
 
     def send(self, text):
-        if self.__is_new():
+        if self._is_new():
             self._send_new(text)
         else:
             self._send_edit(text)
 
-    def __is_new(self):
-        return self.message.message_id is None
+    def _is_new(self):
+        raise NotImplementedError()
 
     def _send_new(self, text):
+        raise NotImplementedError()
+
+    def _send_edit(self, text):
+        raise NotImplementedError()
+
+    def new(self):
+        raise NotImplementedError()
+
+
+class PlainTextReusableMessageSender(ReusableMessageSender):
+    def __init__(self, api: Api, chat_id, separator: str = "\n\n"):
+        super().__init__(api, separator)
+        self.message = Message.create("", chat_id)
+
+    def _is_new(self):
+        return self.message.message_id is None
+
+    def _send_new(self, text: str):
         self.message.new_text(text)
         sent_message = self.api.send_message(self.message)
         self.message.set_message_id(sent_message.message_id)
 
-    def _send_edit(self, text):
+    def _send_edit(self, text: str):
         self.message.new_text(self.message.text + self.separator + text)
         self.api.editMessageText(**self.message.data)
 
     def new(self):
         self.message.set_message_id(None)
+
+
+class FormattedTextReusableMessageSender(ReusableMessageSender):
+    def __init__(self, api: Api, chat_id, separator: FormattedText = FormattedText().newline().newline()):
+        super().__init__(api, separator)
+        self.chat_id = chat_id
+        self.message_id = None
+        self.formatted_text = FormattedText()
+
+    def _is_new(self):
+        return self.message_id is None
+
+    def _send_new(self, formatted_text: FormattedText):
+        message = self.formatted_text.concat(formatted_text).build_message().to_chat(chat_id=self.chat_id)
+        sent_message = self.api.send_message(message)
+        self.message_id = sent_message.message_id
+
+    def _send_edit(self, formatted_text: FormattedText):
+        message = self.formatted_text.concat(self.separator).concat(formatted_text)\
+            .build_message().to_chat(chat_id=self.chat_id)
+        message.set_message_id(self.message_id)
+        self.api.editMessageText(**message.data)
+
+    def new(self):
+        self.formatted_text = FormattedText()
+        self.message_id = None
 
 
 class TimedReusableMessageSender(MessageSender):
