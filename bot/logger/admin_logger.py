29b41ed4c77f559cdb72f1baa0717e3a2fd7a948
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-10-14 17:25:25 +0200

    Use new message senders on AdminLogger

--- bot/logger/admin_logger.py
+++ bot/logger/admin_logger.py
@@ -3,7 +3,9 @@ import traceback
 from bot.action.util.textformat import FormattedText
 from bot.api.api import Api
 from bot.logger.logger import FormattedTextLogger
-from bot.logger.message_sender.reusable.text.formatted import FormattedTextReusableMessageSender
+from bot.logger.message_sender.api import ApiMessageSender
+from bot.logger.message_sender.message_builder.formatted import FormattedTextBuilder
+from bot.logger.message_sender.reusable import ReusableMessageSender
 from bot.logger.message_sender.reusable.timed import TimedReusableMessageSender
 from bot.logger.message_sender.synchronized import SynchronizedMessageSender
 from bot.multithreading.work import Work
@@ -15,7 +17,11 @@ class AdminLogger:
         sender = \
             SynchronizedMessageSender(
                 TimedReusableMessageSender(
-                    FormattedTextReusableMessageSender(api, admin_chat_id),
+                    ReusableMessageSender(
+                        ApiMessageSender(api, admin_chat_id),
+                        FormattedTextBuilder(),
+                        max_length=1000
+                    ),
                     reuse_message_for_seconds=1
                 )
             )
