165b7700377068aa5ac12f90e3900f0748a5233c
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-10-15 20:27:01 +0200

    Create AsynchronousMessageSender to send messages asynchronously using a worker

---
+++ bot/logger/message_sender/asynchronous.py
@@ -0,0 +1,12 @@
+from bot.logger.message_sender import IntermediateMessageSender, MessageSender
+from bot.multithreading.work import Work
+from bot.multithreading.worker import Worker
+
+
+class AsynchronousMessageSender(IntermediateMessageSender):
+    def __init__(self, sender: MessageSender, worker: Worker):
+        super().__init__(sender)
+        self.worker = worker
+
+    def send(self, text):
+        self.worker.post(Work(lambda: self.sender.send(text), "async_message_send"))
