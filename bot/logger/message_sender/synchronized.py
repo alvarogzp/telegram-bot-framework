82756e5314c2768bb3acf03cf542929d23b73f82
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-10-16 18:37:41 +0200

    Use reentrant lock on SynchronizedMessageSender

--- bot/logger/message_sender/synchronized.py
+++ bot/logger/message_sender/synchronized.py
@@ -13,7 +13,12 @@ class SynchronizedMessageSender(IntermediateMessageSender):
 
     def __init__(self, sender: MessageSender):
         super().__init__(sender)
-        self.lock = threading.Lock()
+        # Using a reentrant lock to play safe in case the send function somewhat invokes this send function again
+        # maybe because a send triggers another send on the same message sender.
+        # Note that if this send throws an exception the lock is released when dealing with it from outside,
+        # so this is not a problem.
+        # But if the exception is handled inside this send call, the lock is still hold.
+        self.lock = threading.RLock()
 
     def send(self, text):
         with self.lock:
