56a4553d574253f8b67fcced10989b5ff9c1217d
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-10-18 20:41:11 +0200

    Fix session-related read error when a connection is closed server-side and the notification does not reach the client

--- bot/api/telegram.py
+++ bot/api/telegram.py
@@ -1,6 +1,8 @@
 import threading
 
 import requests
+from requests.adapters import HTTPAdapter
+from urllib3 import Retry
 
 
 class TelegramBotApi:
@@ -30,8 +32,29 @@ class TelegramBotApi:
     def __get_session(self):
         session = self.local.__dict__.get("session")
         if not session:
-            session = requests.session()
+            session = self.__create_session()
             self.local.session = session
+        return session
+
+    def __create_session(self):
+        session = requests.session()
+        # Retry one time on read errors, as the connection could have been closed
+        # by the remote side and its close notification might have been lost,
+        # blocked by firewall or dropped by NAT.
+        # In that case, the session will try to reuse the connection thinking it is
+        # still alive just to fail with a read error when the remote sends the reset.
+        #
+        # So, by retrying in that case we avoid this error and do not lose that api
+        # call, which could be translated in a message lost.
+        # That way we try to get the same behavior as we had when not using sessions.
+        #
+        # The drawback is that we could also be masquerading some *real* read errors
+        # and retrying a request already processed by the server, repeating some
+        # action (eg. a duplicate message).
+        # But, during a year of production use we never had any read error of this kind.
+        retry = Retry(total=1, connect=0, read=1, status=0)
+        # passing prefix lowered to work-around https://github.com/requests/requests/pull/4349
+        session.mount(self.base_url.lower(), HTTPAdapter(max_retries=retry))
         return session
 
     def __log_request(self, request: requests.PreparedRequest):
