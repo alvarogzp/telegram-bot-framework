ed78c5b27356d2f04f95643740b85c216ef20fbb
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-10-19 23:11:57 +0200

    Setup virtualenv before running bot in run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,20 @@
 SLEEP_TIME_SECONDS=30
 DEBUG_ENABLED=true
 
+VIRTUALENV_LOCATION="venv"
+VIRTUALENV_ACTIVATE_SCRIPT_PATH="$VIRTUALENV_LOCATION/bin/activate"
+VIRTUALENV_PYTHON_INTERPRETER="python3"
+VIRTUALENV_CREATE_COMMAND="$VIRTUALENV_PYTHON_INTERPRETER -m venv"
+VIRTUALENV_PYTHON_SCRIPT_TO_CHECK_IF_INSIDE='
+import sys
+prefix = getattr(sys, "real_prefix", None)
+if prefix is not None: sys.exit(0)  # activated
+prefix = getattr(sys, "base_prefix", None)
+if prefix is None: sys.exit(1)  # not activated
+if prefix != sys.prefix: sys.exit(0)  # activated
+sys.exit(1)  # not activated
+'
+
 EXIT_STATUS_TO_HALT_BOT=55
 RUN_BOT_COMMAND="./main.py"
 
@@ -27,6 +41,7 @@ cd_to_current_script_location()
 
 perform_main_tasks()
 {
+    setup_virtualenv
     build_bot
     run_bot
     check_halt_received $?
@@ -34,6 +49,25 @@ perform_main_tasks()
     update_code
     sleep ${SLEEP_TIME_SECONDS}
     rerun_current_script
+}
+
+setup_virtualenv()
+{
+    if [ ! -d "$VIRTUALENV_LOCATION" ]
+    then
+        debug "Creating virtualenv on '$VIRTUALENV_LOCATION'"
+        ${VIRTUALENV_CREATE_COMMAND} "$VIRTUALENV_LOCATION"
+    fi
+    if ! is_virtualenv_activated
+    then
+        debug "Activating virtualenv on '$VIRTUALENV_LOCATION'"
+        . "$VIRTUALENV_ACTIVATE_SCRIPT_PATH"
+    fi
+}
+
+is_virtualenv_activated()
+{
+    "$VIRTUALENV_PYTHON_INTERPRETER" -c "$VIRTUALENV_PYTHON_SCRIPT_TO_CHECK_IF_INSIDE"
 }
 
 build_bot()
