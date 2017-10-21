916820f7887caf5d86eb154528cf2ec569c71add
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-10-21 18:24:23 +0200

    Add /configstatus and /instance commands to manager

--- bot/manager.py
+++ bot/manager.py
@@ -17,8 +17,10 @@ from bot.action.standard.answer import AnswerAction
 from bot.action.standard.benchmark import BenchmarkAction
 from bot.action.standard.chatsettings.action import ChatSettingsAction
 from bot.action.standard.config import ConfigAction
+from bot.action.standard.config_status import ConfigStatusAction
 from bot.action.standard.enterexit import GreetAction, LeaveAction
 from bot.action.standard.gapdetector import GlobalGapDetectorAction
+from bot.action.standard.instance import InstanceAction
 from bot.action.standard.internationalization import InternationalizationAction
 from bot.action.standard.logger import LoggerAction
 from bot.action.standard.perchat import PerChatAction
@@ -127,6 +129,16 @@ class BotManager:
                                             CommandAction("eval").then(
                                                 AdminActionWithErrorMessage().then(
                                                     EvalAction()
+                                                )
+                                            ),
+                                            CommandAction("configstatus").then(
+                                                AdminActionWithErrorMessage().then(
+                                                    ConfigStatusAction()
+                                                )
+                                            ),
+                                            CommandAction("instance").then(
+                                                AdminActionWithErrorMessage().then(
+                                                    InstanceAction()
                                                 )
                                             ),
                                             CommandAction("config").then(
