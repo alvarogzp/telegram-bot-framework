6f1971212f021f27a47644bd4c8017c0a5822046
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-10-25 06:04:11 +0200

    Add worker pool support to scheduler

--- bot/multithreading/scheduler.py
+++ bot/multithreading/scheduler.py
@@ -5,12 +5,14 @@ from bot.multithreading.worker import Worker
 from bot.multithreading.worker.immediate import ImmediateWorker
 from bot.multithreading.worker.queue import QueueWorker
 from bot.multithreading.work import Work
+from bot.multithreading.worker.pool.workers.main import QueueWorkerPool
 
 
 class SchedulerApi:
     def __init__(self, worker_error_handler: callable):
         self.worker_error_handler = worker_error_handler
         self.workers = []
+        self.worker_pools = []
         self.running = False
         self.immediate_worker = ImmediateWorker(worker_error_handler)
         self.network_worker = self._new_worker("network")
@@ -20,6 +22,10 @@ class SchedulerApi:
         worker = QueueWorker(name, queue.Queue(), self.worker_error_handler)
         self.workers.append(worker)
         return worker
+
+    def _new_worker_pool(self, name: str, min_workers: int, max_workers: int, max_seconds_idle: int):
+        return QueueWorkerPool(name, queue.Queue(), self.worker_error_handler, self._start_worker,
+                               min_workers, max_workers, max_seconds_idle)
 
     def setup(self):
         self._start_workers()
@@ -35,6 +41,10 @@ class SchedulerApi:
         thread = threading.Thread(target=worker.run, name=worker.name)
         thread.daemon = True
         thread.start()
+
+    def _start_worker_pool(self, worker: QueueWorkerPool):
+        self.worker_pools.append(worker)
+        worker.start()
 
     def network(self, work: Work):
         self._get_worker(self.network_worker).post(work)
@@ -55,6 +65,19 @@ class SchedulerApi:
         self._start_worker(worker)
         return worker
 
+    def new_worker_pool(self, name: str, min_workers: int, max_workers: int, max_seconds_idle: int):
+        """
+        Creates a new worker pool and starts it.
+        Returns the Worker that schedules works to the pool.
+        """
+        if not self.running:
+            return self.immediate_worker
+        worker = self._new_worker_pool(name, min_workers, max_workers, max_seconds_idle)
+        self._start_worker_pool(worker)
+        return worker
+
     def shutdown(self):
+        for worker in self.worker_pools:
+            worker.shutdown()
         for worker in self.workers:
             worker.shutdown()
