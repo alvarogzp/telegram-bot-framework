4e184c82bcb1277879d711a52c7bc187372af036
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2017-10-25 05:42:10 +0200

    Document that a single QueueWorker instance is safe to be launched multiple times in multiple threads

--- bot/multithreading/worker/__init__.py
+++ bot/multithreading/worker/__init__.py
@@ -48,6 +48,11 @@ class AbstractWorker(Worker):
 
 
 class QueueWorker(AbstractWorker):
+    """
+    Instances of this worker are safe to be running on more than one thread at the same time.
+    The only limitation is that in error handling you will not be able to distinguish between them.
+    """
+
     def __init__(self, name: str, work_queue: queue.Queue, error_handler: callable):
         super().__init__(name, error_handler)
         self.queue = work_queue
