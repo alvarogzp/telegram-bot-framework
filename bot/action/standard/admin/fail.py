ad01adbd2be3cf60a785202e234c88883c865e3f
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2018-04-04 18:08:09 +0200

    Allow simulating async errors

--- bot/action/standard/admin/fail.py
+++ bot/action/standard/admin/fail.py
@@ -4,12 +4,17 @@ from bot.action.util.textformat import FormattedText
 
 class FailAction(Action):
     def process(self, event):
+        api = self.api
         error = NotARealError("simulated error")
         response = FormattedText().bold("Simulating bot error...")
-        if event.command_args == "fatal":
+        args = event.command_args.split()
+        if "fatal" in args:
             error = NotARealFatalError("simulated fatal error")
             response.newline().normal(" - ").bold("FATAL")
-        self.api.send_message(response.build_message().to_chat_replying(event.message))
+        if "async" in args:
+            api = self.api.async
+            response.newline().normal(" - ").bold("async")
+        api.send_message(response.build_message().to_chat_replying(event.message))
         raise error
 
 
