1da9b6538294744aa1d68ec6f48bebbbf5d714bb
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2018-04-04 17:47:43 +0200

    Allow to simulate fatal errors

--- bot/action/standard/admin/fail.py
+++ bot/action/standard/admin/fail.py
@@ -3,8 +3,14 @@ from bot.action.core.action import Action
 
 class FailAction(Action):
     def process(self, event):
+        if event.command_args == "fatal":
+            raise NotARealFatalError("simulated fatal error")
         raise NotARealError("simulated error")
 
 
 class NotARealError(Exception):
+    pass
+
+
+class NotARealFatalError(BaseException):
     pass
