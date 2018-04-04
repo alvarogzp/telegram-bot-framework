36eb0a6e9853bb3be6f3b472da357e4a37396c58
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2018-04-04 18:06:45 +0200

    Send a message previously to simulating fail

--- bot/action/standard/admin/fail.py
+++ bot/action/standard/admin/fail.py
@@ -1,11 +1,16 @@
 from bot.action.core.action import Action
+from bot.action.util.textformat import FormattedText
 
 
 class FailAction(Action):
     def process(self, event):
+        error = NotARealError("simulated error")
+        response = FormattedText().bold("Simulating bot error...")
         if event.command_args == "fatal":
-            raise NotARealFatalError("simulated fatal error")
-        raise NotARealError("simulated error")
+            error = NotARealFatalError("simulated fatal error")
+            response.newline().normal(" - ").bold("FATAL")
+        self.api.send_message(response.build_message().to_chat_replying(event.message))
+        raise error
 
 
 class NotARealError(Exception):
