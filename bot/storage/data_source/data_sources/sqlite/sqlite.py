b8354db8803a9f5d4f9b7a11cd895f683b9dafde
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2018-05-08 23:58:35 +0200

    Add logger to SqliteStorageDataSource

--- bot/storage/data_source/data_sources/sqlite/sqlite.py
+++ bot/storage/data_source/data_sources/sqlite/sqlite.py
@@ -1,12 +1,14 @@
+from sqlite_framework.log.logger import SqliteLogger
 from sqlite_framework.session.session import SqliteSession
 
 from bot.storage.data_source.data_source import StorageDataSource
 
 
 class SqliteStorageDataSource(StorageDataSource):
-    def __init__(self, database_filename: str, debug: bool):
+    def __init__(self, database_filename: str, debug: bool, logger: SqliteLogger):
         super().__init__()
         self.session = SqliteSession(database_filename, debug)
+        self.logger = logger
 
     def init(self):
         self.session.init()
