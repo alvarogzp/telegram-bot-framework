4184d968668ebd590d927aea7ecbaf7088473cb5
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2018-05-17 18:58:35 +0200

    Add create static method to InlineKeyboardButton with all possible parameters

--- bot/action/util/reply_markup/inline_keyboard/button.py
+++ bot/action/util/reply_markup/inline_keyboard/button.py
@@ -3,6 +3,25 @@ class InlineKeyboardButton:
         self.data = data
 
     @staticmethod
+    def create(text: str,
+               url: str = None,
+               callback_data: str = None,
+               switch_inline_query: str = None,
+               switch_inline_query_current_chat: str = None):
+        data = {
+            "text": text
+        }
+        if url is not None:
+            data["url"] = url
+        if callback_data is not None:
+            data["callback_data"] = callback_data
+        if switch_inline_query is not None:
+            data["switch_inline_query"] = switch_inline_query
+        if switch_inline_query_current_chat is not None:
+            data["switch_inline_query_current_chat"] = switch_inline_query_current_chat
+        return InlineKeyboardButton(data)
+
+    @staticmethod
     def switch_inline_query(text: str, query: str = "", current_chat: bool = True):
         switch_inline_query_key = "switch_inline_query_current_chat" if current_chat else "switch_inline_query"
         return InlineKeyboardButton({
