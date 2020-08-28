eeb2a5a0a1ff13f5c933dbe84bae84eb19d5938d
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2020-08-28 06:38:43 +0200

    Lower message retention to reduce server pressure

--- bot/action/extra/messages/storage.py
+++ bot/action/extra/messages/storage.py
@@ -3,8 +3,8 @@ import json
 from bot.action.extra.messages.mapper import StoredMessageMapper
 from bot.action.extra.messages.operations import MessageList, MessageIdOperations
 
-MIN_MESSAGES_TO_KEEP = 1000
-MAX_MESSAGES_TO_KEEP = 10000
+MIN_MESSAGES_TO_KEEP = 50
+MAX_MESSAGES_TO_KEEP = 100
 
 
 class MessageStorageHandler:
